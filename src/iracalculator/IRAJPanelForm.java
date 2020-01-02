09dfbacc2f31b0a4f081f0cf76d478b1366f6274
CodeCrazy97 <evaughan72@yahoo.com>
2020-01-02 11:55:06 -0500

    Use of combo boxes for age instead of text boxes

--- src/iracalculator/IRAJPanelForm.java
+++ src/iracalculator/IRAJPanelForm.java
@@ -17,6 +17,8 @@ public class IRAJPanelForm extends javax.swing.JPanel {
     public IRAJPanelForm() {
         initComponents();
         jPanel2.setVisible(false);
+        retirementAgejComboBox1.setSelectedIndex(64);
+        currentAgejComboBox1.setSelectedIndex(21);
     }
 
     /**
@@ -34,26 +36,26 @@ public class IRAJPanelForm extends javax.swing.JPanel {
         balanceTF = new javax.swing.JTextField();
         jLabel4 = new javax.swing.JLabel();
         roiTF = new javax.swing.JTextField();
-        retirementAgeTF = new javax.swing.JTextField();
-        jLabel7 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
         jLabel6 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
         jLabel5 = new javax.swing.JLabel();
-        yearsToContributeTF = new javax.swing.JTextField();
-        currentAgeTF = new javax.swing.JTextField();
         inflationTF = new javax.swing.JTextField();
         jButton1 = new javax.swing.JButton();
+        retirementAgejComboBox1 = new javax.swing.JComboBox<>();
+        currentAgejComboBox1 = new javax.swing.JComboBox<>();
         jPanel2 = new javax.swing.JPanel();
         jScrollPane1 = new javax.swing.JScrollPane();
         jTextArea1 = new javax.swing.JTextArea();
         jLabel8 = new javax.swing.JLabel();
+        jLabel7 = new javax.swing.JLabel();
+        yearsToContributeTF = new javax.swing.JTextField();
 
         yearlyContributionsTF.setFont(new java.awt.Font("Arial", 1, 18)); // NOI18N
         yearlyContributionsTF.setText("5200");
 
         jLabel1.setFont(new java.awt.Font("Arial", 1, 18)); // NOI18N
-        jLabel1.setText("Current balance:");
+        jLabel1.setText("Current balance ($):");
 
         balanceTF.setFont(new java.awt.Font("Arial", 1, 18)); // NOI18N
         balanceTF.setText("22000");
@@ -64,14 +66,8 @@ public class IRAJPanelForm extends javax.swing.JPanel {
         roiTF.setFont(new java.awt.Font("Arial", 1, 18)); // NOI18N
         roiTF.setText("10.1");
 
-        retirementAgeTF.setFont(new java.awt.Font("Arial", 1, 18)); // NOI18N
-        retirementAgeTF.setText("65");
-
-        jLabel7.setFont(new java.awt.Font("Arial", 1, 18)); // NOI18N
-        jLabel7.setText("Years you contribute till retirement:");
-
         jLabel2.setFont(new java.awt.Font("Arial", 1, 18)); // NOI18N
-        jLabel2.setText("Yearly contributions:");
+        jLabel2.setText("Yearly contributions ($):");
 
         jLabel6.setFont(new java.awt.Font("Arial", 1, 18)); // NOI18N
         jLabel6.setText("Retirement age:");
@@ -81,12 +77,6 @@ public class IRAJPanelForm extends javax.swing.JPanel {
 
         jLabel5.setFont(new java.awt.Font("Arial", 1, 18)); // NOI18N
         jLabel5.setText("Current age:");
-
-        yearsToContributeTF.setFont(new java.awt.Font("Arial", 1, 18)); // NOI18N
-        yearsToContributeTF.setText("28");
-
-        currentAgeTF.setFont(new java.awt.Font("Arial", 1, 18)); // NOI18N
-        currentAgeTF.setText("22");
 
         inflationTF.setFont(new java.awt.Font("Arial", 1, 18)); // NOI18N
         inflationTF.setText("2.75");
@@ -104,12 +94,18 @@ public class IRAJPanelForm extends javax.swing.JPanel {
             }
         });
 
+        retirementAgejComboBox1.setFont(new java.awt.Font("Arial", 1, 18)); // NOI18N
+        retirementAgejComboBox1.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "49", "50", "51", "52", "53", "54", "55", "56", "57", "58", "59", "60", "61", "62", "63", "64", "65", "66", "67", "68", "69", "70", "71", "72", "73", "74", "75", "76", "77", "78", "79", "80", "81", "82", "83", "84", "85", "86", "87", "88", "89", "90", "91", "92", "93", "94", "95", "96", "97", "98", "99", "100" }));
+
+        currentAgejComboBox1.setFont(new java.awt.Font("Arial", 1, 18)); // NOI18N
+        currentAgejComboBox1.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "49", "50", "51", "52", "53", "54", "55", "56", "57", "58", "59", "60", "61", "62", "63", "64", "65", "66", "67", "68", "69", "70", "71", "72", "73", "74", "75", "76", "77", "78", "79", "80", "81", "82", "83", "84", "85", "86", "87", "88", "89", "90", "91", "92", "93", "94", "95", "96", "97", "98", "99", "100" }));
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addContainerGap()
+                .addGap(35, 35, 35)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
@@ -118,19 +114,20 @@ public class IRAJPanelForm extends javax.swing.JPanel {
                             .addComponent(jLabel3)
                             .addComponent(jLabel4)
                             .addComponent(jLabel5)
-                            .addComponent(jLabel6)
-                            .addComponent(jLabel7))
+                            .addComponent(jLabel6))
                         .addGap(18, 18, 18)
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                            .addComponent(yearsToContributeTF)
-                            .addComponent(retirementAgeTF)
-                            .addComponent(currentAgeTF)
                             .addComponent(inflationTF)
                             .addComponent(roiTF)
                             .addComponent(yearlyContributionsTF)
-                            .addComponent(balanceTF, javax.swing.GroupLayout.PREFERRED_SIZE, 118, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                    .addComponent(jButton1, javax.swing.GroupLayout.Alignment.TRAILING))
-                .addContainerGap())
+                            .addComponent(balanceTF)
+                            .addComponent(retirementAgejComboBox1, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(currentAgejComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, 118, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jButton1)
+                        .addGap(76, 76, 76)))
+                .addContainerGap(29, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -152,17 +149,13 @@ public class IRAJPanelForm extends javax.swing.JPanel {
                     .addComponent(inflationTF, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel4))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(currentAgeTF, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabel5))
-                .addGap(20, 20, 20)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(retirementAgeTF, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabel6))
-                .addGap(18, 18, 18)
+                    .addComponent(jLabel5)
+                    .addComponent(currentAgejComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(25, 25, 25)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(yearsToContributeTF, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabel7))
+                    .addComponent(jLabel6)
+                    .addComponent(retirementAgejComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(18, 18, 18)
                 .addComponent(jButton1)
                 .addContainerGap())
@@ -198,25 +191,45 @@ public class IRAJPanelForm extends javax.swing.JPanel {
                 .addContainerGap())
         );
 
+        jLabel7.setFont(new java.awt.Font("Arial", 1, 18)); // NOI18N
+        jLabel7.setText("Number of years you will contribute to IRA:");
+
+        yearsToContributeTF.setFont(new java.awt.Font("Arial", 1, 18)); // NOI18N
+        yearsToContributeTF.setText("28");
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 164, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jLabel7)
+                        .addGap(18, 18, 18)
+                        .addComponent(yearsToContributeTF, javax.swing.GroupLayout.DEFAULT_SIZE, 218, Short.MAX_VALUE)
+                        .addGap(34, 34, 34))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
                 .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap())
+                .addGap(68, 68, 68))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(yearsToContributeTF, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jLabel7))
+                        .addGap(82, 82, 82))))
         );
     }// </editor-fold>//GEN-END:initComponents
 
@@ -225,26 +238,25 @@ public class IRAJPanelForm extends javax.swing.JPanel {
     }//GEN-LAST:event_inflationTFActionPerformed
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+
         double realROI = Double.parseDouble(roiTF.getText());  // realROI - the IRA's interest rate minus inflation rate.
         realROI -= Double.parseDouble(inflationTF.getText());
         realROI /= 100.0;
 
         double balance = Double.parseDouble(balanceTF.getText());  // balance - the amount of money in the IRA
         double beginningBalance = balance;                         // beginningBalance - stores the initial amount of money in the IRA
-        int age = Integer.parseInt(currentAgeTF.getText());        // age - the current age of the user in years
-        int retirementAge = Integer.parseInt(retirementAgeTF.getText()); // retirementAge - age at which the user plans on retiring
+        int age = Integer.parseInt("" + currentAgejComboBox1.getSelectedItem());        // age - the current age of the user in years
+        int retirementAge = Integer.parseInt("" + retirementAgejComboBox1.getSelectedItem()); // retirementAge - age at which the user plans on retiring
 
         int yearsToContribute = Integer.parseInt(yearsToContributeTF.getText());  // yearsToContribute - the number of years the user will contribute to the IRA
         double yearlyContributions = Double.parseDouble(yearlyContributionsTF.getText());
         double totalContributions = 0.0;
 
-        
-        boolean interestAlert = false;  // Tells whether user has been informed of interest being greater than contributions.
-        
         jPanel2.setVisible(true);  // Unhide the panel that shows an overview of IRA.
+        jTextArea1.setText("");
         jTextArea1.append("(Contributions shown below do not include beginning balance.)\n");
         jTextArea1.append(" Initial balance: $" + (int) balance + "\n");
-        while (age != retirementAge){
+        while (age != retirementAge) {
             balance *= (1.0 + realROI);
             if (yearsToContribute > 0) {
                 totalContributions += yearlyContributions;
@@ -254,17 +266,12 @@ public class IRAJPanelForm extends javax.swing.JPanel {
             age++;
             String balanceWithCommas = createStringWithCommas((int) balance);
             String contributionsWithCommas = createStringWithCommas((int) (totalContributions));
-            if (!interestAlert && ((int) totalContributions + beginningBalance) < ((int) balance - beginningBalance)) {
-                jTextArea1.append(" Interest beats contributions at age " + age + "!!!\n");
-                jTextArea1.append(" IRA worth at age " + age + ": $" + balanceWithCommas);
-                jTextArea1.append(". Contributions: $" + contributionsWithCommas + "\n");
-                interestAlert = true;  
-            } else if (age < retirementAge) {  
+            if (age < retirementAge) {
                 jTextArea1.append(" IRA worth at age " + age + ": $" + balanceWithCommas);
                 jTextArea1.append(". Contributions: $" + contributionsWithCommas + "\n");
             } else {  // Reached retirement age.
                 jTextArea1.append("---------------------------------------------------------------------------------------");
-                jTextArea1.append(" \nIRA worth at age " + age + ": $" + balanceWithCommas + ". Total contributions: $" + contributionsWithCommas);
+                jTextArea1.append("\n IRA worth at age " + age + ": $" + balanceWithCommas + ". Total contributions: $" + contributionsWithCommas);
             }
         }
         jTextArea1.append("\n");
@@ -286,7 +293,7 @@ public class IRAJPanelForm extends javax.swing.JPanel {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JTextField balanceTF;
-    private javax.swing.JTextField currentAgeTF;
+    private javax.swing.JComboBox<String> currentAgejComboBox1;
     private javax.swing.JTextField inflationTF;
     private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
@@ -301,7 +308,7 @@ public class IRAJPanelForm extends javax.swing.JPanel {
     private javax.swing.JPanel jPanel2;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTextArea jTextArea1;
-    private javax.swing.JTextField retirementAgeTF;
+    private javax.swing.JComboBox<String> retirementAgejComboBox1;
     private javax.swing.JTextField roiTF;
     private javax.swing.JTextField yearlyContributionsTF;
     private javax.swing.JTextField yearsToContributeTF;
